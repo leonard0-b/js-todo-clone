d705bd773d27fdc7b04e8da5252a8360e23a37de
Leonardo <leonardo.bongianni@gmail.com>
2021-03-17 18:32:38 +0100

    Esercizio Completo con Bonus
    Il css lo faccio ne we promesso 😎

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,45 @@
 // Creare una Todo-list in cui si possano aggiungere nuovi todo e rimuoverli.
 // Rendiamo il tutto gradevole alla vista.
+
 $(document).ready(function(){
+  // creo funzione al click🐱‍👤
   $("#add").click(function(){
+    // mi salvo il valore dell'input🐱‍👤
     var listVal = $("#input-list").val();
+    $("#input-list").val("");
+    // clono la lista che voglio ripetere🐱‍👤
     var listItem = $("#list-item li").clone();
 
+    // seleziono i figli della lista con classe "title" e scrivo nello span il valore dell'input🐱‍👤
     listItem.children("span.title").text(listVal);
+    // lo stampo nella sezione list🐱‍👤
     $("#list").append(listItem);
 
+    // al click sullo span con classe "elimina"🐱‍👤
     $(".elimina").click(function(){
+      // elimina il parente superiore (li)🐱‍👤
       $(this).parent().remove();
     })
+
+    // al click su "modifica" il testo si nasconde e appare un form🐱‍👤
+    $(".modifica").click(function(){
+      // click su modifica si mostra il suo prev🐱‍👤
+      $(this).prev().show();
+      // si nasconde suo fratello con classe "title"🐱‍👤
+      $(this).siblings(".title").hide();
+      // il form prende il valore da modificare🐱‍👤
+      $(this).siblings(".edit").val(listVal);
+
   })
 
-
-
-
-
-
-
-
-
-
-
+    // dopo la modifica premendo invio la modifica è ultimata🐱‍👤
+    listItem.children(".edit").keyup(function(e){
+      if (e.which == 13) {
+        var title = $(this).val();
+        $(this).hide();
+        $(this).siblings("span.title").text(title).show();
+        listVal = title;
+      }
+    })
+  })
 })
