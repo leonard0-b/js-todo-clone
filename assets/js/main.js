8381ff7b616caae69868a2fddc822933fddac533
Leonardo <leonardo.bongianni@gmail.com>
2021-03-17 15:20:22 +0100

    Inizio es

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,12 @@
 // Rendiamo il tutto gradevole alla vista.
 $(document).ready(function(){
   $("#add").click(function(){
-    console.log("ok");
   var listVal = $("#input-list").val();
   var listItem = $("#list-item li").clone();
 
-  // listItem.children("span.title").text(listVal);
-  listItem.children(".title").text(listVal);
-  console.log(listItem.children(".title"));
+  listItem.children().text(listVal);
 
   })
-
-
-
-
-
-
 
 
 })
